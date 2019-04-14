138dbe452b6bc8a5d5458f3d3d9310971b2d07eb
franksam007 <franksam007@gmail.com>
2019-04-14 23:10:34 +0800

    根据使用场景修改accession的中文翻译，bootstrap.datepicker汉化

--- frontend/vendor/assets/javascripts/bootstrap-datepicker.js
+++ frontend/vendor/assets/javascripts/bootstrap-datepicker.js
@@ -1740,7 +1740,7 @@
 		forceParse: true,
 		format: 'mm/dd/yyyy',
 		keyboardNavigation: true,
-		language: 'en',
+		language: 'zh',
 		minViewMode: 0,
 		maxViewMode: 4,
 		multidate: false,
@@ -1780,6 +1780,16 @@
 			today: "Today",
 			clear: "Clear",
 			titleFormat: "MM yyyy"
+		},
+		zh: {
+			days: ["周日", "周一", "周二", "周三", "周四", "周五", "周六"],
+			daysShort: ["日", "一", "二", "三", "四", "五", "六"],
+			daysMin: ["日", "一", "二", "三", "四", "五", "六"],
+			months: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
+			monthsShort: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
+			today: "今天",
+			clear: "清除",
+			titleFormat: "yyyy MM"
 		}
 	};
 
